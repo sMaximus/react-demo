a4b2e277145d77b97ca53321e3d6e6405de2da4d
zhangjiaming <zhangjiaming@sursenelec.com>
2024-03-07 00:17:38 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+/*
+ * @Author: zhangjiaming zhangjiaming@sursenelec.com
+ * @Date: 2024-03-06 21:43:11
+ * @LastEditors: zhangjiaming zhangjiaming@sursenelec.com
+ * @LastEditTime: 2024-03-07 00:15:40
+ * @FilePath: /react-demo/src/App.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>1</>;
 }
 
 export default App;
