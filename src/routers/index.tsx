ac4cd8888c2bcdd8bf9872075ad8531a0ff78028
zhangjiaming <zhangjiaming1>
2024-03-13 18:00:34 +0800

    router

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,19 +1,31 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "@pages/home";
 import Login from "@pages/login";
+import NotFound from "../pages/notFound";
+import Layout from "@src/layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/home" />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
+    element: <Layout />,
+    action: () => {
+      console.warn(879);
+      return 121;
+    },
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+    ],
   },
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
