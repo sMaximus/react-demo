8244c7099bcc85ccbe45aa9d95bdce2d79013d54
zhangjiaming <zhangjiaming1>
2024-03-14 18:02:20 +0800

    feature : add layout

---
+++ src/layout/modules/Item.tsx
@@ -0,0 +1,31 @@
+// import { theme } from "antd";
+// import { css } from "@emotion/css";
+
+// const Item: React.FC<{ children: React.ReactNode }> = (props) => {
+//   const { token } = theme.useToken();
+//   return (
+//     <div
+//       className={css`
+//         color: ${token.colorTextSecondary};
+//         font-size: 14px;
+//         cursor: pointer;
+//         line-height: 22px;
+//         margin-bottom: 8px;
+//         &:hover {
+//           color: ${token.colorPrimary};
+//         }
+//       `}
+//       style={{
+//         width: "33.33%",
+//       }}>
+//       {props.children}
+//       <DoubleRightOutlined
+//         style={{
+//           marginInlineStart: 4,
+//         }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Item;
