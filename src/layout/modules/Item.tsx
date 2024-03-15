e12398afe52cf61f70e7bbe46f742d4e275e1686
zhangjiaming <zhangjiaming1>
2024-03-15 18:10:12 +0800

    feature

--- src/layout/modules/Item.tsx
+++ src/layout/modules/Item.tsx
@@ -1,31 +1,32 @@
-// import { theme } from "antd";
-// import { css } from "@emotion/css";
+import { theme } from "antd";
+import { css } from "@emotion/css";
+import { DoubleRightOutlined } from "@ant-design/icons";
 
-// const Item: React.FC<{ children: React.ReactNode }> = (props) => {
-//   const { token } = theme.useToken();
-//   return (
-//     <div
-//       className={css`
-//         color: ${token.colorTextSecondary};
-//         font-size: 14px;
-//         cursor: pointer;
-//         line-height: 22px;
-//         margin-bottom: 8px;
-//         &:hover {
-//           color: ${token.colorPrimary};
-//         }
-//       `}
-//       style={{
-//         width: "33.33%",
-//       }}>
-//       {props.children}
-//       <DoubleRightOutlined
-//         style={{
-//           marginInlineStart: 4,
-//         }}
-//       />
-//     </div>
-//   );
-// };
+const Item: React.FC<{ children: React.ReactNode }> = (props) => {
+  const { token } = theme.useToken();
+  return (
+    <div
+      className={css`
+        color: ${token.colorTextSecondary};
+        font-size: 14px;
+        cursor: pointer;
+        line-height: 22px;
+        margin-bottom: 8px;
+        &:hover {
+          color: ${token.colorPrimary};
+        }
+      `}
+      style={{
+        width: "33.33%",
+      }}>
+      {props.children}
+      <DoubleRightOutlined
+        style={{
+          marginInlineStart: 4,
+        }}
+      />
+    </div>
+  );
+};
 
-// export default Item;
+export default Item;
